740bc9896db9a80c9d0361e93186aa318cc5f4b1
yarinmatmoni <yarinmatmoni@gmail.com>
2022-12-22 00:39:25 +0200

    change homepage link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,93 +3,109 @@ import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router';
 //Import style
 import styled from 'styled-components';
-import {motion} from "framer-motion";
+import { motion } from 'framer-motion';
 
 function Nav() {
-    const {pathname} = useLocation();
-    return (
-        <StyledNav>
-            <h1><Link id="logo" to="/">Capture</Link></h1>
-            <ul>
-                <li>
-                    <Link to="/">1. About Us</Link>
-                    <Line transition={{duration:0.75}} initial={{width:"0%"}} animate={{width: pathname === "/" ? "45%" : "0%"}}/>
-                </li>
-                <li>
-                    <Link to="/work">2. Our Work</Link>
-                    <Line transition={{duration:0.75}} initial={{width:"0%"}} animate={{width: pathname === "/work" ? "45%" : "0%"}}/>
-                </li>
-                <li>
-                    <Link to="/contact">3. Contact Us</Link>
-                    <Line transition={{duration:0.75}} initial={{width:"0%"}} animate={{width: pathname === "/contact" ? "45%" : "0%"}}/>
-                </li>
-            </ul>
-        </StyledNav>
-    )
-};
+	const { pathname } = useLocation();
+	return (
+		<StyledNav>
+			<h1>
+				<Link id="logo" to="/">
+					Capture
+				</Link>
+			</h1>
+			<ul>
+				<li>
+					<Link to="/capturePortfilio">1. About Us</Link>
+					<Line
+						transition={{ duration: 0.75 }}
+						initial={{ width: '0%' }}
+						animate={{ width: pathname === '/' ? '45%' : '0%' }}
+					/>
+				</li>
+				<li>
+					<Link to="/work">2. Our Work</Link>
+					<Line
+						transition={{ duration: 0.75 }}
+						initial={{ width: '0%' }}
+						animate={{ width: pathname === '/work' ? '45%' : '0%' }}
+					/>
+				</li>
+				<li>
+					<Link to="/contact">3. Contact Us</Link>
+					<Line
+						transition={{ duration: 0.75 }}
+						initial={{ width: '0%' }}
+						animate={{ width: pathname === '/contact' ? '45%' : '0%' }}
+					/>
+				</li>
+			</ul>
+		</StyledNav>
+	);
+}
 
 const StyledNav = styled.div`
-    min-height: 10vh;
-    display: flex;
-    margin: auto;
-    justify-content: space-between;
-    align-items: center;
-    padding: 1rem 8rem;
-    background: #282828;
-    position: sticky;
-    top:0;
-    z-index: 10;
+	min-height: 10vh;
+	display: flex;
+	margin: auto;
+	justify-content: space-between;
+	align-items: center;
+	padding: 1rem 8rem;
+	background: #282828;
+	position: sticky;
+	top: 0;
+	z-index: 10;
 
-    a{
-        color: white;
-        text-decoration: none;
-    }
+	a {
+		color: white;
+		text-decoration: none;
+	}
 
-    ul{
-        display: flex;
-        list-style: none;
-    }
+	ul {
+		display: flex;
+		list-style: none;
+	}
 
-    #logo {
-        font-size: 1.5rem;
-        font-family: "Lobster",cursive;
-        font-weight: lighter;
-    }
+	#logo {
+		font-size: 1.5rem;
+		font-family: 'Lobster', cursive;
+		font-weight: lighter;
+	}
 
-    li{
-        padding-left: 10rem;
-        position: relative;
-    }
+	li {
+		padding-left: 10rem;
+		position: relative;
+	}
 
-    @media (max-width:1300px){
-        flex-direction: column;
-        padding: 2rem 1rem;
-        #logo{
-            display: inline-block;
-            margin: 1rem;
-        }
-        ul{
-            padding:2rem;
-            justify-content: space-around;
-            width: 100%;
-        }
-        li{
-            padding: 0;
-        }
-    }
+	@media (max-width: 1300px) {
+		flex-direction: column;
+		padding: 2rem 1rem;
+		#logo {
+			display: inline-block;
+			margin: 1rem;
+		}
+		ul {
+			padding: 2rem;
+			justify-content: space-around;
+			width: 100%;
+		}
+		li {
+			padding: 0;
+		}
+	}
 `;
 
 const Line = styled(motion.div)`
-    height: 0.3rem;
-    background: #23d997;
-    width: 0%;
-    position: absolute;
-    bottom: -80%;
-    left: 60%;
+	height: 0.3rem;
+	background: #23d997;
+	width: 0%;
+	position: absolute;
+	bottom: -80%;
+	left: 60%;
 
-    @media (max-width : 1300px){
-        left: 0%;
-    }
+	@media (max-width: 1300px) {
+		left: 0%;
+	}
 `;
 
 export default Nav;
